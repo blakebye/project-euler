93fe8e5e1466ef5dd9706118496ede291da6c3d6
Blake Bye <blake.bye@gmail.com>
2014-09-28 12:24:24 -0700

    make sieve more efficient

--- euler007.py
+++ euler007.py
@@ -7,12 +7,12 @@ import math
 
 TEST_VALUE = 10001
 
-# 1.15 is cutting it close, only works for > 2000
-# 2.5 is safe for everything
-EFFICIENCY_COEFFICIENT = 1.15
-
+# 1.15 is cutting it close, only works for value > 2000
+# 2.5 is safe for all values
 if TEST_VALUE < 2000:
     EFFICIENCY_COEFFICIENT = 2.5
+else:
+    EFFICIENCY_COEFFICIENT = 1.15
 
 
 def generate_prime(value):
@@ -21,13 +21,15 @@ def generate_prime(value):
     It uses the prime number theorem to overshot an estimate
     of the prime and then tests up to that overshot.
     """
-    estimate = int(EFFICIENCY_COEFFICIENT * value * math.log(value))
-    primality = [False] * 2 + [True] * estimate
     prime_list = list()
+    estimate = int(EFFICIENCY_COEFFICIENT * value * math.log(value))
+    primality = [False] * 2 + [True] * (estimate - 2)
+    for non_prime in range(4, estimate, 2):
+                primality[non_prime] = False
     for factor in range(len(primality)):
         if primality[factor]:
             prime_list.append(factor)
-            for non_prime in range(factor ** 2, estimate, factor):
+            for non_prime in range(factor ** 2, estimate, factor * 2):
                 primality[non_prime] = False
     return prime_list[value - 1]
 

20665f341ae59243bb26c5c6b79e232e64e6e0ba
Blake Bye <blake.bye@gmail.com>
2014-10-01 00:44:36 -0700

    9.2e-04 seconds

--- euler017.py
+++ euler017.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-This module solves Project Euler #16
+This module solves Project Euler #17
 """
 
 MAP = {0: "", 1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six",

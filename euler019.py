942f40821f2c320066ef39523913bd128d3a8668
Blake Bye <blake.bye@gmail.com>
2014-10-01 01:28:44 -0700

    7.55e-04 seconds

--- euler019.py
+++ euler019.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-This module solves Project Euler #1
+This module solves Project Euler #19
 """
 
 from datetime import date, timedelta
@@ -8,18 +8,28 @@ from datetime import date, timedelta
 ONE_DAY = timedelta(days=1)
 ONE_WEEK = timedelta(days=7)
 
+
 def count_sunday_the_firsts(start, end):
-    date = start
+    """
+    This function takes a start date and end date and finds
+    all of the sundays that land on the first of the month
+    within the allotted period.
+    """
+    day_to_check = start
     total_sundays = int()
-    while date.weekday() != 6:
-        date += ONE_DAY
-    while date < end:
-        if date.day == 1:
+    while day_to_check.weekday() != 6:
+        day_to_check += ONE_DAY
+    while day_to_check < end:
+        if day_to_check.day == 1:
             total_sundays += 1
-        date += ONE_WEEK
+        day_to_check += ONE_WEEK
     return total_sundays
 
+
 def main():
+    """
+    Solves the problem.
+    """
     start_date = date(1901, 1, 1)
     end_date = date(2000, 12, 31)
     return count_sunday_the_firsts(start_date, end_date)

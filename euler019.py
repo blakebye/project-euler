aee5d3b82ce910559a2b59f4b7e642d76f3e65f1
Blake Bye <blake.bye@gmail.com>
2014-10-01 04:32:33 -0700

    2.09e-04 seconds

--- euler019.py
+++ euler019.py
@@ -7,6 +7,14 @@ from datetime import date, timedelta
 
 ONE_DAY = timedelta(days=1)
 ONE_WEEK = timedelta(days=7)
+TWO_WEEKS = timedelta(days=14)
+THREE_WEEKS = timedelta(days=21)
+FOUR_WEEKS = timedelta(days=28)
+FIVE_WEEKS = timedelta(days=35)
+SIX_WEEKS = timedelta(days=42)
+SEVEN_WEEKS = timedelta(days=49)
+EIGHT_WEEKS = timedelta(days=56)
+NINE_WEEKS = timedelta(days=63)
 
 
 def count_sunday_the_firsts(start, end):
@@ -20,9 +28,28 @@ def count_sunday_the_firsts(start, end):
     while day_to_check.weekday() != 6:
         day_to_check += ONE_DAY
     while day_to_check < end:
-        if day_to_check.day == 1:
+        day = day_to_check.day
+        if day == 1:
             total_sundays += 1
-        day_to_check += ONE_WEEK
+            day_to_check += FOUR_WEEKS
+        elif day <= 4:
+            day_to_check += FOUR_WEEKS
+        elif day <= 7:
+            day_to_check += EIGHT_WEEKS
+        elif day <= 11:
+            day_to_check += THREE_WEEKS
+        elif day <= 14:
+            day_to_check += SEVEN_WEEKS
+        elif day <= 18:
+            day_to_check += TWO_WEEKS
+        elif day <= 21:
+            day_to_check += SIX_WEEKS
+        elif day <= 25:
+            day_to_check += ONE_WEEK
+        elif day <= 28:
+            day_to_check += FIVE_WEEKS
+        else:
+            day_to_check += NINE_WEEKS
     return total_sundays
 
 

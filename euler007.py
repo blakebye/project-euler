599593ff8af8246addd4741c72e4ffb6159066bc
Blake Bye <blake.bye@gmail.com>
2014-10-01 02:49:28 -0700

    .024 seconds

--- euler007.py
+++ euler007.py
@@ -5,27 +5,18 @@ This module solves Project Euler #7
 
 import math
 
-TEST_VALUE = 10001
 
-# 1.15 is cutting it close, only works for value > 2000
-# 2.5 is safe for all values
-if TEST_VALUE < 2000:
-    EFFICIENCY_COEFFICIENT = 2.5
-else:
-    EFFICIENCY_COEFFICIENT = 1.15
-
-
-def generate_prime(value):
+def generate_prime(value, efficiency):
     """
     This function takes a value and tries to find that prime.
     It uses the prime number theorem to overshot an estimate
     of the prime and then tests up to that overshot.
     """
     prime_list = list()
-    estimate = int(EFFICIENCY_COEFFICIENT * value * math.log(value))
+    estimate = int(efficiency * value * math.log(value))
     primality = [False] * 2 + [True] * (estimate - 2)
     for non_prime in range(4, estimate, 2):
-                primality[non_prime] = False
+        primality[non_prime] = False
     for factor in range(len(primality)):
         if primality[factor]:
             prime_list.append(factor)
@@ -33,4 +24,19 @@ def generate_prime(value):
                 primality[non_prime] = False
     return prime_list[value - 1]
 
-print generate_prime(TEST_VALUE)
+
+def main():
+    """
+    Solves the problem.
+    """
+    test_value = 10001
+    # 1.15 is cutting it close, only works for value > 2000
+    # 2.5 is safe for all values
+    if test_value < 2000:
+        efficiency_coefficient = 2.5
+    else:
+        efficiency_coefficient = 1.15
+    return generate_prime(test_value, efficiency_coefficient)
+
+if __name__ == "__main__":
+    print main()
